6e92fa3419b39538e79efcd5dd9ef73e82e505dd
Rachel <g00337231@gmit.ie>
2019-11-04 20:27:18 +0000

    read fist 3 numbers

--- mnistreader-c/read.c
+++ mnistreader-c/read.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[])
 {
     // open the file
     // rb - read as a binary file
     FILE *f = fopen("../files/t10k-images-idx3-ubyte", "rb");
-    __uint8_t b; //_t = type uint8 = 8 bit unassigned integer  
-    int i, j;
+    uint8_t b; //_t = type uint8 = 8 bit unassigned integer  
+    int i, j, k;
 
     // read a bit at a time from the file
     for(i = 0; i < 16; i++) //first 20 bits
@@ -15,6 +16,24 @@ int main(int argc, char *argv[])
         printf("%02x ", b);
     }
 
+    printf("/n");
+
+    for(k = 0; k < 3; k++)
+    {
+        for(i = 0; i < 28; i++ )
+        {
+            for(j = 0; j < 28; j++)
+            {
+                fread(&b, 1, 1, f);
+                printf("%s", (b > 127) ? "0" : "."); // is the current bit > 127 ( not a white space)
+                
+            }
+
+            printf("\n");
+        }
+        printf("\n");
+    }
+    
     printf("\n");
     return 0;
 }
