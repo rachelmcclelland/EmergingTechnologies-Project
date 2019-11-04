df985e4612fb1f28096bf84c6e47f7ad4f5ffe7d
Rachel <g00337231@gmit.ie>
2019-11-04 20:17:04 +0000

    read in first 16 bits

--- mnistreader-c/read.c
+++ mnistreader-c/read.c
@@ -5,13 +5,16 @@ int main(int argc, char *argv[])
     // open the file
     // rb - read as a binary file
     FILE *f = fopen("../files/t10k-images-idx3-ubyte", "rb");
-    
+    __uint8_t b; //_t = type uint8 = 8 bit unassigned integer  
+    int i, j;
 
     // read a bit at a time from the file
-    for(int i = 0; i < 20; i++) //first 20 bits
+    for(i = 0; i < 16; i++) //first 20 bits
     {
-        fread(&, 1, 1, f);
+        fread(&b, 1, 1, f);
+        printf("%02x ", b);
     }
 
+    printf("\n");
     return 0;
 }
