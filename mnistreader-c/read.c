723e14c6fa871f29e9a6d3c3ad22982393c92e82
Rachel <g00337231@gmit.ie>
2019-11-04 20:34:42 +0000

    read in the labels for first three images

--- mnistreader-c/read.c
+++ mnistreader-c/read.c
@@ -6,7 +6,9 @@ int main(int argc, char *argv[])
     // open the file
     // rb - read as a binary file
     FILE *f = fopen("../files/t10k-images-idx3-ubyte", "rb");
-    uint8_t b; //_t = type uint8 = 8 bit unassigned integer  
+    FILE *labels = fopen("../files/t10k-labels-idx1-ubyte", "rb");
+
+    uint8_t b, lblB; //_t = type uint8 = 8 bit unassigned integer  
     int i, j, k;
 
     // read a bit at a time from the file
@@ -16,10 +18,22 @@ int main(int argc, char *argv[])
         printf("%02x ", b);
     }
 
-    printf("/n");
+    printf("\n");
+
+    // read in the first part of the labels file
+    for(i = 0; i < 8; i ++)
+    {
+        fread(&lblB, 1, 1, labels);
+        printf("%02x ", lblB);
+    }
+
+    printf("\n");
 
     for(k = 0; k < 3; k++)
     {
+        fread(&lblB, 1, 1, labels);
+        printf("%02x \n", lblB);
+
         for(i = 0; i < 28; i++ )
         {
             for(j = 0; j < 28; j++)
